d34241a652a04e7c6f04bc8e8e3c29981ecdd93b
Elder-Ferreira <ferreira3.elder@gmail.com>
2023-10-13 18:40:53 -0300

    Criando lista de tarefas

--- todolist/src/main/java/br/com/elder/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/elder/todolist/user/UserController.java
@@ -1,5 +1,7 @@
 package br.com.elder.todolist.user;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
@@ -21,9 +25,14 @@ public class UserController {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if(user != null) {
-            System.out.println("Usuaário já existe");
+            System.out.println("Usuário já existe");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var passwordHashred = BCrypt.withDefaults()
+        .hashToString( 12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
